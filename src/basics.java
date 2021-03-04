f59d1a3c38a8858e63c933c30caea1e2057c3128
MedSdiri <sdiri.ad@gmail.com>
2021-03-04 00:44:43 -0600

    Tadaa

--- src/basics.java
+++ src/basics.java
@@ -23,7 +23,14 @@ public class basics {
 		.header("Server","Apache/2.4.18 (Ubuntu)" );
 		
 		// Add - place -> Update Place with new Address 
-		//-> Get place to validate if the new address is present in response  
+		//-> Get place to validate if the new address is present in response
+		// 03/03/2021 2:07 am, we will finish this work tomorrow
+		//calling it for the night ? btw this is tomorrow but we didn't finish this work lol
+		// we focused a bit on databases with the group we will get back here tomorrow yes if we are not in 
+		//yesterday's tomorrow now it will be tomorrow's tomorrow we will finish this stuff up 
+		//just felt like typing some stuff here and use the git bash to send it to my remote repo
+		//so if u are reading this and laughing already thinking that I'm crazy !!
+		//you bett ure ass I'm crazy allez bonne nuit les amis @ demain :D 
 		
 		
 		
